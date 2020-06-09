547386b6dec81d4b54d8b1bd3773ef074110bfc8
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-09 19:02:25 +0200

    tbc

--- scripts.js
+++ scripts.js
@@ -1,16 +1,35 @@
 let data = {
-    photo: '1monthsleep.jpg',
+    photo: '1month.jpg',
     title: 'One month old Levi',
-    description: 'This is how he slept at one month old'
+    description: 'This is how he slept at one month old',
 };
 
+
+
+
+/*let data = {
+    photo: ['1month.jpg', '3month.jpg', '6month.jpg', '10month.jpg', '1yr.jpg'],
+    title: ['One month old Levi', 'Three months old Levi', 'Six months old Levi', 'Ten months old Levi'],
+    description: ['This is how he slept at one month old',
+                'This is how he slept at three months old',
+                'This is how he slept at six months old',
+                'This is how he slept at ten months old']
+};*/
+
+let currentPhoto = 0;
+var currentPhotoTitle = 0;
+var currentPhotoDescription = 0;
+
 $('#photo').attr('src', data.photo);
-$('#photo-title').attr('src', data.title);
-$('#photo-description').attr('src', data.description);
+$('#photo-title').text(data.title);
+$('#photo-description').text(data.description);
 
+/*let loadPhoto = (photoNumber) => {
 
-/*<img id="photo" src="1monthsleep.jpg">
-    <div class="gray-container">
-      <h1 id="photo-title">One month old Levi</h1>
-      <p id="photo-description">This is how he slept at one month old</p>
-    </div>*/
+    $('#photo').attr('src', data.photo[currentPhoto]);
+    $('#photo').css('border', 'solid 2px white');
+    $('#photo').css('box-shadow', '0 0 50px #fff');
+    $('#currentPhotoTitle').text(data.title[currentPhotoTitle]);
+    $('#currentPhotoDescription').text(data.description[currentPhotoDescription]);
+
+}*/
