ae5c07fc876f7f596dc0b86365d9e355bfe865d0
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-10 15:15:36 +0200

    finished third day exercise

--- scripts.js
+++ scripts.js
@@ -1,35 +1,55 @@
-let data = {
+/*let data = {
     photo: '1month.jpg',
     title: 'One month old Levi',
     description: 'This is how he slept at one month old',
-};
-
-
-
-
-/*let data = {
-    photo: ['1month.jpg', '3month.jpg', '6month.jpg', '10month.jpg', '1yr.jpg'],
-    title: ['One month old Levi', 'Three months old Levi', 'Six months old Levi', 'Ten months old Levi'],
-    description: ['This is how he slept at one month old',
-                'This is how he slept at three months old',
-                'This is how he slept at six months old',
-                'This is how he slept at ten months old']
 };*/
 
+
+
+
+let imagesData = {
+    photo: ['1month.jpg', '3month.jpg', '6month.jpg', '10month.jpg', '1yr.jpg'],
+    title: ['One month old Levi', 'Three months old Levi', 'Six months old Levi', 'Ten months old Levi', 'One year old Levi'],
+    description: ['This is how he slept when he was one month old',
+                'This is how he slept when he was three months old',
+                'This is how he slept when he was six months old',
+                'This is how he slept when he was ten months old',
+                'This is how he slept when he was one year old']
+};
+
 let currentPhoto = 0;
-var currentPhotoTitle = 0;
-var currentPhotoDescription = 0;
+let currentPhotoTitle = 0;
+let currentPhotoDescription = 0;
 
-$('#photo').attr('src', data.photo);
+/*$('#photo').attr('src', data.photo);
 $('#photo-title').text(data.title);
-$('#photo-description').text(data.description);
+$('#photo-description').text(data.description);*/
 
-/*let loadPhoto = (photoNumber) => {
+let loadPhoto = (photoNumber) => {
 
-    $('#photo').attr('src', data.photo[currentPhoto]);
-    $('#photo').css('border', 'solid 2px white');
-    $('#photo').css('box-shadow', '0 0 50px #fff');
-    $('#currentPhotoTitle').text(data.title[currentPhotoTitle]);
-    $('#currentPhotoDescription').text(data.description[currentPhotoDescription]);
+    $('#mainImg').attr('src', imagesData.photo[currentPhoto]);
+   // $('#mainImg').css('border', 'solid 2px grey');
+   // $('#mainImg').css('box-shadow', '5 5 50px #fff');
+    $('#currentPhotoTitle').text(imagesData.title[currentPhotoTitle]);
+    $('#currentPhotoDescription').text(imagesData.description[currentPhotoDescription]);
+}
 
-}*/
+loadPhoto(currentPhoto)
+
+$('#right-arrow').click(() => {
+    if(currentPhoto < 5) {
+        currentPhoto++; 
+        currentPhotoTitle++;
+        currentPhotoDescription++;
+    }
+    loadPhoto(currentPhoto);
+});
+
+$('#left-arrow').click(() => {
+    if(currentPhoto > 0) {
+        currentPhoto--; 
+        currentPhotoTitle--;
+        currentPhotoDescription--;
+    }
+    loadPhoto(currentPhoto);
+});
