0c3ba29339b2690fda6234d7d9688314a744916d
Noemike <66320530+Noemike@users.noreply.github.com>
2020-06-11 19:51:51 +0200

    awesome

--- scripts.js
+++ scripts.js
@@ -1,68 +1,64 @@
-/*let data = {
+let image1 = {
     photo: '1month.jpg',
     title: 'One month old Levi',
-    description: 'This is how he slept at one month old',
-};*/
+    description: 'This is how he slept when he was one month old',
+}
 
-let imagesData = {
-    photo: ['1month.jpg', '3month.jpg', '6month.jpg', '10month.jpg', '1yr.jpg'],
-    title: ['One month old Levi', 'Three months old Levi', 'Six months old Levi', 'Ten months old Levi', 'One year old Levi'],
-    description: ['This is how he slept when he was one month old',
-                'This is how he slept when he was three months old',
-                'This is how he slept when he was six months old',
-                'This is how he slept when he was ten months old',
-                'This is how he slept when he was one year old']
-};
+let image2 = {
+    photo: '3month.jpg',
+    title: 'Three month old Levi',
+    description: 'This is how he slept when he was three months old',
+}
+
+let image3 = {
+    photo: '6month.jpg',
+    title: 'Six month old Levi',
+    description: 'This is how he slept when he was six months old',
+}
+
+let image4 = {
+    photo: '10month.jpg',
+    title: 'Ten month old Levi',
+    description: 'This is how he slept when he was ten months old',
+}
+
+let image5 = {
+    photo: '1yr.jpg',
+    title: 'One year old Levi',
+    description: 'This is how he slept when he was one year old',
+}
+
+let imagesData = [image1, image2, image3, image4, image5]
+
 
 let currentPhoto = 0;
-let currentPhotoTitle = 0;
-let currentPhotoDescription = 0;
 
-/*$('#photo').attr('src', data.photo);
-$('#photo-title').text(data.title);
-$('#photo-description').text(data.description);*/
 
 let loadPhoto = (photoNumber) => {
 
-    $('#mainImg').attr('src', imagesData.photo[currentPhoto]);
-    $('#currentPhotoTitle').text(imagesData.title[currentPhotoTitle]);
-    $('#currentPhotoDescription').text(imagesData.description[currentPhotoDescription]);
+    $('#mainImg').attr('src', imagesData[photoNumber].photo);
+    $('#currentPhotoTitle').text(imagesData[photoNumber].title);
+    $('#currentPhotoDescription').text(imagesData[photoNumber].description);
 }
 
 loadPhoto(currentPhoto)
 
+imagesData.forEach((thumbnail, index) => {
+    $(".thumbnail-container").append(`<img onclick="loadPhoto(${index})" src="${thumbnail.photo}"/>`)
+
+});
+
 $('#right-arrow').click(() => {
-    if(currentPhoto < 5) {
-        currentPhoto++; 
-        currentPhotoTitle++;
-        currentPhotoDescription++;
+    if (currentPhoto < 5) {
+        currentPhoto++;
     }
     loadPhoto(currentPhoto);
 });
 
 $('#left-arrow').click(() => {
-    if(currentPhoto > 0) {
-        currentPhoto--; 
-        currentPhotoTitle--;
-        currentPhotoDescription--;
+    if (currentPhoto > 0) {
+        currentPhoto--;
     }
     loadPhoto(currentPhoto);
 });
 
-/*for(let i = 1; i <= imagesData.photo.length; i++) {
-    let numberIndex = parseInt(currentPhoto+1);
-
-    $('.thumbnail-container').append('<div class="thumbnail"><img src="'+i+'.jpg" class="thumbnailImg"><div class="hiddenThumbnailTitle"></div></div>');  
-    $('.thumbnail[src="'+numberIndex+'.jpg"]').css('border', 'solid 2px white');
-    $('.thumbnail[src="'+numberIndex+'.jpg"]').css('box-shadow', '0 0 50px #fff');
-}
-    
-  let data = ['first', 'second', 'third', 'fourth', 'fifth'] ;
-
-  data.forEach ((item, index) => {
-      $('.thumbnail-container').append(`<div class="box" data-index="${index}">${item} (data-index="${index}")</div>`);
-      $('.box').click((event) => {
-        let indexClicked = $(event.target).attr('data-index');
-    }
-  
-  );*/
